9353dbb5d1b4ab09b4ffe1980ecf081c93c9de90
_Imran Kayes_ <emrankays1@gmail.com>
2025-02-02 00:43:00 +0600

    selenium 1st assignment

--- src/test/java/selenium1/LaunchBrowser.java
+++ src/test/java/selenium1/LaunchBrowser.java
@@ -1,14 +1,21 @@
 package selenium1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https://mvnrepository.com/search?q=selenium");
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#login-button")).click();
+        Thread.sleep(2000);
         driver.quit();
 
     }
